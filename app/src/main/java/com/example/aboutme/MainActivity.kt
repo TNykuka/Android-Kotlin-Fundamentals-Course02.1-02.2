f646f68e507f1b8633a7968c02f61d031728cdb5
TNy-Kk <kingkuka088@gmail.com>
2020-07-19 01:50:49 +0700

    Android Kotlin Fundamentals 02.2-8 Step1

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             addNickname(it)
         }
 
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
+
     }
     private fun addNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
@@ -32,6 +36,15 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
 
+    }
+
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 
 
