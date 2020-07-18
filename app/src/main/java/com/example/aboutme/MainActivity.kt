71970b57fc11adda5d10c4a6afcb1982db087ea5
TNy-Kk <kingkuka088@gmail.com>
2020-07-19 01:48:59 +0700

    Android Kotlin Fundamentals 02.2-7 Step3

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
     }
     private fun addNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
@@ -21,9 +28,10 @@ class MainActivity : AppCompatActivity() {
         view.visibility = View.GONE
         nicknameTextView.visibility = View.VISIBLE
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it)
-        }
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 
 
